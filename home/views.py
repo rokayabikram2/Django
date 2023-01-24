dea2c337add7f9ca56f1a3109d516e9910dc6c3c
rokayabikram2 <rokayabikram2@gmail.com>
2023-01-24 16:16:59 +0545

    index file and static file

--- home/views.py
+++ home/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def price(request):
+    return render(request, 'index.html')
+
+
+def services(request):
+    return render(request, 'index.html')
+
+
+
